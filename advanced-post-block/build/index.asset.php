33812bc70f5b68c02f77cb541d654adb98a6c16f
Sean Murphy <sean@iamseanmurphy.com>
2026-09-06 14:10:39 +0000

    2.5.0

--- advanced-post-block/build/index.asset.php
+++ advanced-post-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1ab7feeb85fd70098564');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2c0e7019cf2e0143984e');
